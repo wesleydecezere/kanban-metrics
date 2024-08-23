0e11a58e101a74b01c337daa3f14ff31a3939fb7
wesleydecezere <wesleydecezere@gmail.com>
2024-08-23 10:02:21 -0300

    Rm console.log

--- prisma/operations/sprint.ts
+++ prisma/operations/sprint.ts
@@ -1,8 +1,6 @@
 import { prisma } from "../client/client";
 
 export const createSprint = () => {
-  console.log(Object.keys(prisma));
-
   return prisma.sprint.create({
     data: {
       name: "foo",
